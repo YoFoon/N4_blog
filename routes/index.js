e0cf5a7b284ef9a6e0a7c813b45155e1df68ca63
YoFoon <605022360@qq.com>
2015-11-03 16:08:47 +0800

    增加标签和标签页面

--- routes/index.js
+++ routes/index.js
@@ -114,7 +114,8 @@ module.exports = function(app) {
   app.post('/post', checkLogin);
 app.post('/post', function (req, res) {
   var currentUser = req.session.user,
-      post = new Post(currentUser.name, req.body.title, req.body.post);
+    tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+    post = new Post(currentUser.name, req.body.title, tags, req.body.post);
   post.save(function (err) {
     if (err) {
       req.flash('error', err); 
@@ -162,7 +163,36 @@ app.post('/post', function (req, res) {
       });
     });
   });
-
+  app.get('/tags', function (req, res) {
+    Post.getTags(function (err, posts) {
+      if (err) {
+        req.flash('error', err); 
+        return res.redirect('/');
+      }
+      res.render('tags', {
+        title: '标签',
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
+  app.get('/tags/:tag', function (req, res) {
+    Post.getTag(req.params.tag, function (err, posts) {
+      if (err) {
+        req.flash('error',err); 
+        return res.redirect('/');
+      }
+      res.render('tag', {
+        title: 'TAG:' + req.params.tag,
+        posts: posts,
+        user: req.session.user,
+        success: req.flash('success').toString(),
+        error: req.flash('error').toString()
+      });
+    });
+  });
   app.get('/u/:name',function(req,res){
     var page = req.query.p ? parseInt(req.query.p) : 1;
     //检查用户所在
